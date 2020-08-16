962232a68f65bd487a1b453d0c139d166a64595a
Sergey <SK.marv1n.minky@gmail.com>
2020-08-16 07:44:18 +0300

    windowed version+change license

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 <p align='center'>%+'c'</p>
+
+
+
+requarements
+mint/ubuntu/....
+sudo apt-get install pyqt5 <b>or</b>python3 -m install pyqt5
+python 3.8:
+<blockquote>python3 idontknowwhy.py</blockquote> 
+
+bash: 
+./idonknowwhy
+
+
---
+++ idonknowwhy.py
@@ -0,0 +1,68 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget,QLabel, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QGridLayout, QLineEdit
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+class App(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Gray Stuff'
+        self.left = 10
+        self.top = 10
+        self.width = 180
+        self.height = 100
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.createGridLayout()
+        
+        windowLayout = QVBoxLayout()
+        windowLayout.addWidget(self.horizontalGroupBox)
+        self.setLayout(windowLayout)
+
+        
+        self.show()
+    
+
+    def createGridLayout(self):
+        self.horizontalGroupBox = QGroupBox("XO")
+        layout = QGridLayout()
+        layout.setColumnStretch(1, 2)
+        layout.setColumnStretch(1, 4)
+        button = QPushButton('click')
+
+
+        self.a = QLineEdit(self)
+        self.to_hex = QLineEdit(self)
+        
+        layout.addWidget(QLabel("Type your script:"),0,0)
+        layout.addWidget(self.a,0,1)
+        layout.addWidget(button,1,1)
+        layout.addWidget(self.to_hex,2,1)
+        layout.addWidget(QLabel("Copy to clipboard:"),2,0)
+        layout.addWidget(QLabel(""),3,0)
+        
+
+        self.horizontalGroupBox.setLayout(layout)
+        button.clicked.connect(self.slot_method)
+    @pyqtSlot()
+    def slot_method(self):
+    	b = self.a.text()
+    	hex_str = ""
+
+    	
+    	for i in range(len(b)):
+    		hex_str +="%"+str(b[i].encode("utf-8").hex())
+
+    	self.to_hex.setText(hex_str)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
